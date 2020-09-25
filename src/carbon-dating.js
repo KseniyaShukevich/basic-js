0834e8d5408310eb2fecc81ead34dccf23d0c14b
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2020-09-25 04:16:14 +0300

    feat: add carbon-dating and count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,20 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
+const LOG_OF_TWO =  Math.log(2);
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let years = 0;
+  let type = typeof +sampleActivity;
+  reg = /^[0-1.]+$/;
+
+  if (type === 'Number' && reg.test(sampleActivity)) {
+    let yearsBuff = (Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD) / LOG_OF_TWO;
+    years = Math.ceil(yearsBuff);
+
+  return years;
+  }
+
+  return false;
 };

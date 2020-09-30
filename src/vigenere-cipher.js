9f4dbeba2526b1859395bc74b07b22f012fd9b34
Shukevich Kseniya <Kseniya.Shukevich@gmail.com>
2020-09-30 18:34:39 +0300

    feat: finally I'm done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,78 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(flag = true) {
+    this.flag = flag;
+  }
+
+  _encrypt(message, key) {
+    if (message && key) {
+      let result = '';
+
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+
+        let countKey = 0;
+
+        for (let i = 0; i < message.length; i++) {
+          let charCode = '';
+
+          if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+            charCode = ( message.charCodeAt(i) + key.charCodeAt(countKey % key.length) - 130 ) % 26 + 65;
+            result += String.fromCharCode(charCode);
+            countKey++;
+          } else {
+            result += message[i];
+          }
+        }
+
+        return result;
+    }
+
+    throw 'Error';
+  }
+
+  encrypt(message, key) {
+    if (this.flag) {
+      return this._encrypt(message, key);
+    } else {
+      return this._encrypt(message, key).split('').reverse().join('');
+    }
+  }
+
+  _decrypt(message, key) {
+    if (message && key) {
+      let result = '';
+
+      message = message.toUpperCase();
+      key = key.toUpperCase();
+
+        let countKey = 0;
+
+        for (let i = 0; i < message.length; i++) {
+          let charCode = '';
+
+          if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+            charCode = ( message.charCodeAt(i) + 26 - key.charCodeAt(countKey % key.length) ) % 26 + 65;
+            result += String.fromCharCode(charCode);
+            countKey++;
+          } else {
+            result += message[i];
+          }
+        }
+
+        return result;
+    }
+
+    throw 'Error';
+  }
+
+  decrypt (message, key) {
+    if (this.flag) {
+      return this._decrypt(message, key);
+    } else {
+      return this._decrypt(message, key).split('').reverse().join('');
+    }
   }
 }
 
